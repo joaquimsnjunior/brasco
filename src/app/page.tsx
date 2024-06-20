7a239f592aba0388179923857b0ce08554f02821
joaqumsnjunior <joaquimsilvanetojunior@gmail.com>
2024-06-20 16:11:59 -0300

    style: troca de favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
 import Stats from "@/components/hero/Stats";
 import Announcement from "@/components/ui/Announcement";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
